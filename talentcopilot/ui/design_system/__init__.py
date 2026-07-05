1934b23f5e8487f40bc0cd8197038e8cbe398457
ljulienne <louisjulienne1987@gmail.com>
2026-07-05 06:28:03 +0000

    LANG-02: Add reusable page header component

--- talentcopilot/ui/design_system/__init__.py
+++ talentcopilot/ui/design_system/__init__.py
@@ -4,3 +4,5 @@ from .evidence_card import render_evidence_card
 from .risk_card import render_risk_card
 from .interview_focus_card import render_interview_focus_card
 from .decision_center import render_decision_center
+
+from .page_header import render_page_header
